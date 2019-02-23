81438c67d8f2a81e90a27c4af72cd07a6a13cffd
Karyna Bialkovich <karibel@mail.ru>
2019-02-23 20:08:44 +0300

    solution_1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig) 
+{
+
+  let check, str_2;
+
+ do{
+    check = false;
+
+    bracketsConfig.forEach(function(item) {
+      str_2 = item.join('');
+
+      if (str.includes(str_2)) {
+        str = str.replace(str_2, '');
+        check = true;
+      }
+      
+    });
+  } while (check)
+
+  if (!str.length) return true;
+    else return false;
 }
+
